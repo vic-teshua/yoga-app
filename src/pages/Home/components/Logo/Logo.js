66172f55259d8954ab043c4f3415cf574e45aed1
Boruchigor <boruchigor@gmail.com>
2021-05-12 11:48:29 +0200

    Update Logo.js

--- src/pages/Home/components/Logo/Logo.js
+++ src/pages/Home/components/Logo/Logo.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import video1 from '../../../../videos/VicClub.mp4';
 import './Logo.css'
+import {Link} from 'react-router-dom'
 const Logo = () => {
     return (
         <div className="container">
         <video autoPlay src={video1} type="video/mp4" muted></video>
         <h3 className="intro">Design Your Daily Yoga Practice</h3>
-        <button className="button">Start now</button>
+        <button className="button">
+        <Link to='/design'>
+        Start now
+        </Link>
+        </button>
+        
         </div>
     )
 }
