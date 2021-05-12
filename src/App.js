3a7ad6fc0ba9154b016b79de056dcb1adae40a59
Stefan Reimer <72360122+stefanreimer777@users.noreply.github.com>
2021-05-12 11:20:35 +0200

    update test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Logo from './pages/Home/components/Logo/Logo'
-import './App.css';
-import Navigation from './pages/Home/components/Navigation/Navigation';
-import Footer from './pages/Home/components/Footer/Footer';
+import Logo from "./pages/Home/components/Logo/Logo";
+import "./App.css";
+import Navigation from "./pages/Home/components/Navigation/Navigation";
+import Footer from "./pages/Home/components/Footer/Footer";
+import Contact from "./pages/Contact/ContactForm";
+
 function App() {
   return (
     <div className="App">
       <Navigation />
-     <Logo />
-     <Footer />
+      <Logo />
+      <Footer />
     </div>
   );
 }
