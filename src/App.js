f90639a0c9369139992357eaf1bb5a63a4dfda6e
LiamTimlin <williamtimlin@outlook.com>
2021-05-12 15:28:54 +0200

    moved

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Logo from "./pages/Home/components/Logo/Logo";
 import "./App.css";
 import Navigation from "./pages/Home/components/Navigation/Navigation";
 import Footer from "./pages/Home/components/Footer/Footer";
-import About from "./pages/Home/components/About/About";
+import About from "./pages/About/About";
 function App() {
     return (
         <div className="App">
