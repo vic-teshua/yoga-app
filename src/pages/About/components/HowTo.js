6d6b6f49ba95764604887a3521b4fae12ca9ac0b
Boruchigor <boruchigor@gmail.com>
2021-05-12 15:43:52 +0200

    Update HowTo.js

--- src/pages/About/components/HowTo.js
+++ src/pages/About/components/HowTo.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 
 function HowTo({ number, step }) {
     return (
         <div>
             <h3>
-                {number} <FontAwesomeIcon icon={faCoffee} />
+                {number} <i className="fa fa-coffee"/>
             </h3>
 
             <p>{step}</p>
