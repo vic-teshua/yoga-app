bb6e1f476d61502cc9047d424fb3c7bcf2fe47d1
Boruchigor <boruchigor@gmail.com>
2021-05-17 14:53:32 +0200

    updated homepage

--- src/pages/Home/components/Footer/Footer.js
+++ src/pages/Home/components/Footer/Footer.js
@@ -4,7 +4,17 @@ const Footer = () => {
     return (
         <div className="footer">
             <h2> Created by <br></br> Project-XxX Team</h2>
-            <h3>Connect: <i class="fab fa-twitter-square"></i> <i class="fab fa-facebook-square"></i> <i class="fab fa-instagram-square"></i></h3>
+            <h3>Connect: <a href="https://twitter.com" target='_blank' rel="noreferrer">
+            <i class="fab fa-twitter-square"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+             <i class="fab fa-instagram-square"></i>
+             </a>
+             </h3>
+
         </div>
     )
 }
