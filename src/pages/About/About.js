b180cacb8cc40893d5b756e0533b053789e8a264
Boruchigor <boruchigor@gmail.com>
2021-05-18 10:21:05 +0200

    about update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,36 +6,118 @@ import HowToList from "./components/HowToList";
 function About() {
     return (
         <div>
-            <h2>About Us</h2>
-            <section className="aboutUsSection">
-                <img
-                    src="https://images.unsplash.com/photo-1552196527-bffef41ef674?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHlvZ2F8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                />
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quaerat, deserunt temporibus autem natus quae, minus alias amet, veniam
-                    omnis aspernatur placeat doloribus id. Nisi ipsa ducimus reprehenderit soluta beatae! Lorem ipsum, dolor sit amet consectetur adipisicing
-                    elit.
-                    <br />
-                    <br />
-                    Nemo dolor unde, distinctio perferendis itaque aspernatur id mollitia doloremque. Quaerat delectus eos voluptatum eveniet error aliquam
-                    voluptas ullam consequatur natus eum.
-                </p>
-            </section>
+            <section className="aboutUsSection ">
+                <hr className="featurette-divider" />
 
-            <section>
-                <h3>History behind the health</h3>
-                <div className="historySection">
-                    <History />
-                    <History />
-                    <History />
+                <div className="row featurette">
+                    <div className="col-md-6">
+                        <h2 className="featurette-heading">Yoga - strike the pose!</h2>
+                        <p className="lead">
+                            In a world of hundreds of objects, thousands of tasks and uncountable things needing your attention… <br />
+                            You need just one thing - Yoga!
+                            <br />
+                            <br /> But where do you start? Online, the library, a groovy guru?
+                            <br />
+                            <br /> We’d like to help! <br />
+                            XXX Yoga App, makes things simple with an easy to use drag and drop card system. <br />
+                            <br />
+                            Giving you control and letting you Yoga - without the stress.
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <img
+                            className="featurette-image img-fluid mx-auto"
+                            xmlns="http://www.w3.org/2000/svg"
+                            role="img"
+                            aria-label="Placeholder: 500x500"
+                            preserveAspectRatio="xMidYMid slice"
+                            focusable="false"
+                            src="https://images.unsplash.com/photo-1603988363607-e1e4a66962c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                            alt=""
+                        />
+                    </div>
                 </div>
             </section>
 
-            <section>
-                <h3>How to use the app</h3>
-                <div className="howToSection">
+            {/* HISTORY SECTION */}
+
+            <section className="aboutUsSection ">
+                <hr className="featurette-divider" />
+
+                <div className="row featurette d-flex ">
+                    <div className="col-md-6">
+                        <div className="col-md-6 d-flex flex-row">
+                            <img
+                                className="featurette-image img-fluid mx-auto"
+                                xmlns="http://www.w3.org/2000/svg"
+                                role="img"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                                src="https://images.unsplash.com/photo-1456406644174-8ddd4cd52a06?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3RyZXNzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                                alt=""
+                            />
+                            <img
+                                className="featurette-image img-fluid mx-auto"
+                                xmlns="http://www.w3.org/2000/svg"
+                                role="img"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                                src="https://images.unsplash.com/photo-1517898717281-8e4385a41802?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHN0cmVzc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                                alt=""
+                            />
+                        </div>
+                        <div className="col-md-6 d-flex flex-row">
+                            <img
+                                className="featurette-image img-fluid mx-auto"
+                                xmlns="http://www.w3.org/2000/svg"
+                                role="img"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                                src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fHlvZ2F8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                                alt=""
+                            />
+                            <img
+                                className="featurette-image img-fluid mx-auto"
+                                xmlns="http://www.w3.org/2000/svg"
+                                role="img"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                                src="https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3RyZXNzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <h2 className="featurette-heading">Where we’re coming from.</h2>
+                        <p className="lead">
+                            We love Yoga!
+                            <br />
+                            It relieves stress, is a great work out and brightens our daily life.
+                            <br />
+                            <br /> However not all of us have the time or space to do yoga in a meaningful way. <br />
+                            The answer to this, came from the creation of the XXX Yoga App.
+                            <br />
+                            <br /> Our idea was to let people get inspired and decide on a routine that would be make yoga work for them, and fit into their
+                            lives.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="container-fluid">
+                <h3 className="text-center">How to use the app</h3>
+                <div>
                     <HowToList />
+                </div>
+
+                <div className="row">
+                    <div className="col text-center p-1 mb-5 ">
+                        <button className="btn btn-primary p-3 ">Use the App</button>
+                    </div>
                 </div>
             </section>
         </div>
