7d2189b06809a6b6ca4bc4ddd79704a003600ada
LiamTimlin <williamtimlin@outlook.com>
2021-05-18 10:28:44 +0200

    Updated HowToList

--- src/pages/About/components/HowToList.js
+++ src/pages/About/components/HowToList.js
@@ -3,16 +3,19 @@ import HowTo from "./HowTo";
 
 const howToData = [
     {
+        id: 1,
         title: "1. Search and Select Yoga Cards",
         caption: "There are over 40 unique yoga poses to use.  Finding the right ones is easy, with the ability to search by name or difficulty.",
         img: "fas fa-search fa-7x",
     },
     {
+        id: 2,
         title: "2. Drag and Drop cards",
         caption: "Like a pose? Drag and drop a card to create your own yoga work out. Click on the card to get instructions and even benefits.",
         img: "far fa-hand-pointer fa-7x",
     },
     {
+        id: 3,
         title: "3. You’re Done",
         caption:
             "With your workout created, you can immediately start to do what you love - Yoga! You can also tweak and reorder the cards, to get an even better fit.",
