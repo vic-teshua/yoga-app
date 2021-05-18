13be5fc894413bf238c615d886c134e538ce90e7
LiamTimlin <williamtimlin@outlook.com>
2021-05-18 13:55:04 +0200

    about page style update

--- src/pages/About/components/HowTo.js
+++ src/pages/About/components/HowTo.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../About.css";
 
 function HowTo({ title, caption, img }) {
     return (
-        <div className="col-lg-3  ">
-            <div className="d-flex flex-column gap-5  bg-white p-5 text-center ">
+        <div className="col-lg-3 row howCard">
+            <div className="d-flex flex-column gap-5  bg-white p-5 text-center  ">
                 <i className={img} alt="" />
-                <h4>{title}</h4>
+                <h4 className="fw-bold">{title}</h4>
                 <p>{caption}</p>
             </div>
         </div>
